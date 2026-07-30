bd75ed92f9ac2ad155f6deab180f93adfea74632
Will <will.odonnell@gmail.com>
2026-07-29 19:10:47 -0700

    Remove obsolete website bundle

--- mid-year-review-sites-bundle/website/server/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const response = await env.ASSETS.fetch(request)
-    const { pathname } = new URL(request.url)
-    if (response.status !== 404 || request.method !== 'GET' || pathname.includes('.')) return response
-    return env.ASSETS.fetch(new Request(new URL('/index.html', request.url), request))
-  },
-}
